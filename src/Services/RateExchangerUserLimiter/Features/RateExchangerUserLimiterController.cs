de2daf7949e94e13e87392545a5197b0d01660a6
Blerim Bojaxhi <blerim.bojaxhi@gig-ext.com>
2022-09-19 16:23:46 +0200

    Refactor

--- src/Services/RateExchangerUserLimiter/Features/RateExchangerUserLimiterController.cs
+++ src/Services/RateExchangerUserLimiter/Features/RateExchangerUserLimiterController.cs
@@ -1,19 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RateExchangerUserLimiter.Models;
 using System.Net;
 
 namespace RateExchangerUserLimiter.Extensions
 {
 
     [ApiController]
-    [Route("[controller]")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/[controller]")]
     public class RateExchangerUserLimiterController : ControllerBase
     {
-        [HttpGet]
+        [HttpPost]
         [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
-        public IActionResult Test()
+        public IActionResult GetExchangeRateUser([FromBody] UserMessage userMessage)
         {
-            return Ok("This Works!");
+            if (userMessage == null) throw new ArgumentNullException("message is null");
+
+            return Ok(userMessage);
         }
 
         [HttpGet("ping")]
---
+++ src/Services/RateExchangerUserLimiter/Models/UserMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateExchangerUserLimiter.Models
+{
+    public class UserMessage
+    {
+        public int MessageId { get; set; }
+        public int UserId { get; set; }
+        public int Amount { get; set; }
+        public string FromCurrencyCode { get; set; }
+        public string ToCurrencyCode { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
